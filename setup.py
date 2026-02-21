e299e7eb94feca7d6cc1f022e21453f123ee9665
Clara Tamura <clarant2@uw.edu>
2026-02-21 00:22:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='AIvsHuman',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         # "numpy==1.26.4",   # if you must pin for Colab compatibility
